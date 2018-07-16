beda60f2b067e350523d3c7159a8e5d5297f4e1f
Oskarzito <oskar.kv@gmail.com>
2018-07-16 10:58:26 +0200

    3.1 HTML och CSS validerar. Bör kontrolleras om allt är uppfyllt rent funktionellt

--- 3.1/scripts/main.js
+++ 3.1/scripts/main.js
@@ -5,6 +5,17 @@
     I framtida uppgifter kanske jag kör getElementById istället för querySelector.
     Huvudsakligen kommer jag vara konsekvent samt hålla mig till en
     i respektive uppgift, och endast mixa om det krävs.
+
+    This JS class gives behaviour to assignment 3.1 Grunderna i JavaScript.
+    It makes use of the following JS entities:
+    - Array     <
+    - Boolean   <
+    - Date      <
+    - Math      <
+    - Number    <
+    - String    <
+    - RegExp    <
+    - Global    <
 */
 
 var ADD_BUTTON = '[data-btn-role="addButton"]';
@@ -15,57 +26,85 @@ var INPUT_NUMBER_TEXTBOX = '[data-input-role="inputNumber"]';
 var INPUT_NAME_TEXTBOX = '[data-input-role="inputName"]';
 var CONFIRM_NAME_BUTTON = '[data-btn-role="nameButton"]';
 var OUTPUT_TABLE = '[data-table-role="outputTable"]';
-var SHOW_ERROR = 'wrong-input';
+var NUMBER_ERROR_PARAGRAPH = '[data-error-role="numberError"]';
+var NAME_ERROR_PARAGRAPH = '[data-error-role="nameError"]';
+var SHOW_ERROR = 'show-error-message';
 var inputArray = [];
 
+/*
+    Gives behaviour to the button 'Addera!'. It takes the value in the
+    textbox, checks if it is a valid number
+*/
 function addButtonListener() {
     'use strict';
+    //Reference to the button
     var button = document.querySelector(ADD_BUTTON);
+
+    //Add click listener
     button.addEventListener('click', function(event) {
         event.preventDefault();
         console.log('Add was clicked');
-        var input = document.querySelector(INPUT_NUMBER_TEXTBOX).value;
+
+        /*References the input textbox's value. By using the Number object, the input
+        also accepts binary and hexadecimal input when converting from a string to
+        a number*/
+        var input = new Number(document.querySelector(INPUT_NUMBER_TEXTBOX).value);
 
         /* Could have been written without Boolean() function, but written with
         because the assignmen said I should use the JS Boolean object.
-        Better safe than sorry :) */
+        Better safe than sorry :)
+        isNaN is a Global function*/
         if(Boolean(isNaN(input) || input === "")){
-            document.body.classList.add(SHOW_ERROR);
+            /*Adds a class to the error message paragraph so a CSS selector
+            matches and shows an error*/
+            document.querySelector(NUMBER_ERROR_PARAGRAPH).classList.add(SHOW_ERROR);
         } else {
-            document.body.classList.remove(SHOW_ERROR);
+            //Removes the error message (unmatches a CSS selector)
+            document.querySelector(NUMBER_ERROR_PARAGRAPH).classList.remove(SHOW_ERROR);
+
             var inputAsNumber;
 
+            //Checks if input is a Float or an Integer
             if(isFloat(input)) {
                 inputAsNumber = parseFloat(input);
             } else {
                 inputAsNumber = parseInt(input);
             }
 
+            //Adds number to array
             inputArray.push(inputAsNumber);
-            writeToScreen(input, 'p');
+
+            /*Calls a function to print input to screen.
+            Arguments passed are the input and what type of HTML element
+            it should be displayed as*/
+            writeToScreen(inputAsNumber, 'p');
         }
     });
 }
 
 /*
-    Checks if a string is an integer.
+    Checks if a string is an Integer
 */
 function isInt(number) {
     'use strict';
+    //Makes use of the global object
     return parseInt(number) == number;
 }
 
 /*
-    Checks if a string is a float.
+    Checks if a string is a Float
 */
 function isFloat(number) {
     'use strict';
-    /*I don't use === because number will always be a string.
+    /*I don't use === because input number will always be a string.
     This way, I parse it to a float and check if it's the same WITH
-    type conversion.*/
+    type conversion. Also, this makes use of the global object*/
     return parseFloat(number) == number;
 }
 
+/*
+    Calculates and returns the sum of all numbers currently entered.
+*/
 function getCurrentSum() {
     'use strict';
     var sum = 0;
@@ -78,65 +117,160 @@ function getCurrentSum() {
 function addCalculateButtonListener() {
     'use strict';
     var button = document.querySelector(CALCULATE_BUTTON);
+
+    //Add click listener
     button.addEventListener('click', function(event) {
+        //Prevents default anchor tag behaviour
         event.preventDefault();
         console.log('Calculate was Clicked!!');
-        var sum = getCurrentSum();
-        var outputString = "Summan av alla talen som matades in är: " + sum;
+
+        //Makes a string of a text and the current total sum of all values entered
+        var outputString = "Summan av alla talen som matades in är: " + getCurrentSum();
+
+        //Writes the string to the screen as a paragraph tag
         writeToScreen(outputString, 'p');
     });
 }
 
+/*
+    Gives functionality to the 'Slumpmässigt tal!' button. Pressing the button
+    generates a random number and adds it to the total sum (or rather the
+    array of all input numbers)
+*/
 function addRandomButtonListener() {
     'use strict';
+    //Reference to the button
     var button = document.querySelector(RANDOM_BUTTON);
+
+    //Add click listener
     button.addEventListener('click', function(event) {
+        //Prevents default anchor tag behaviour
         event.preventDefault();
         console.log('Random clicked!!');
+
+        //Calculates current total sum
         var sum = getCurrentSum();
+
+        /*Generates a random number between 0 and the current total sum.
+        With 'Math.floor' we avoid long decimal numbers (only generates Integers)*/
         var rndNum = Math.floor(Math.random() * sum);
+
+        //Add to input array (contains all entered values)
         inputArray.push(rndNum);
+
+        //Write the number to the screen as a paragraph
         writeToScreen(rndNum, 'p');
     });
 }
 
-/*Writes to the output div tag. whatToWrite = what's written.
-element = as what kind of tag the output should be*/
+/*Writes to the output div tag. whatToWrite = what's going to be written.
+element = as what kind of tag the output should be (for this program it's
+always paragraphs)*/
 function writeToScreen (whatToWrite, element) {
     'use strict';
+    //Reference to the output div tag
     var output = document.querySelector(OUTPUT_DIV);
 
+    //Creates new element
     var htmlElement = document.createElement(element);
+
+    //Writes to the new elemet
     htmlElement.innerHTML = whatToWrite;
+
+    //Gives a class to the element
     htmlElement.classList.add('output-value-paragraph');
 
-    //output.appendChild(htmlElement);
-
+    /*Displays the new element above the others making it look nice and
+    readable in the output div box*/
     output.insertBefore(htmlElement, output.firstChild);
 }
 
+/*
+    Adds a listener to the 'Bekräfta namn' button. It validates the name, and
+    if OK, writes out name and total sum to the table. If name isn't OK, it
+    displays an error message
+*/
 function addNameButtonListener() {
     'use strict';
+    //Reference to the button
     var button = document.querySelector(CONFIRM_NAME_BUTTON);
+
+    //Add click listener
     button.addEventListener('click', function(event) {
+        //Prevents the anchor tag to reference elsewhere
         event.preventDefault();
         console.log('Confirm name clicked!!');
+
+        //Gets the inputed value in the textbox
         var name = document.querySelector(INPUT_NAME_TEXTBOX).value;
-        var sum = getCurrentSum();
-        appendToTable(name, sum);
+
+        //Checks name validity
+        if(isValidName(name)){
+            //Removes the error message (unmatches a CSS selector)
+            document.querySelector(NAME_ERROR_PARAGRAPH).classList.remove(SHOW_ERROR);
+
+            //Prints name and sum to the table
+            appendToTable(name, getCurrentSum());
+
+            //Clears the array
+            inputArray = [];
+        } else {
+            console.log('Invalid name: ' + name);
+            /*Gets a reference to the error messsage paragraph and givs it a class.
+            The CSS class styles the message red and makes it visible*/
+            document.querySelector(NAME_ERROR_PARAGRAPH).classList.add(SHOW_ERROR);
+        }
     });
 }
 
-function appendToTable(name, sum){
+/*
+    Validates a string to see if it matches a certain RegEx pattern (see below)
+*/
+function isValidName(name) {
     'use strict';
-    var table = document.querySelector(OUTPUT_TABLE);
-    var row = table.insertRow(table.rows.length);
-    var cell1 = row.insertCell(0);
-    cell1.innerHTML = name;
-    var cell2 = row.insertCell(1);
-    cell2.innerHTML = sum;
+    //Remove whitespace before and after the string
+    name = name.trim();
+
+    /*Regex matching capital first letter followed by any
+    number of capital letters, lower case letters and numbers interchangably,
+    within a range of 2-12*/
+    var regex = /^[A-Z]+[A-z0-9]{2,12}$/;
+
+    //Returns a boolean of whether the string matches the RegEx pattern or not
+    return regex.test(name);
 }
 
+/*
+    Adds a new row to the table a the bottom. The new row
+    consist of 3 cells, the first will contain the name, the
+    second the sum, and the third todays date
+*/
+function appendToTable(name, sum){
+    'use strict';
+    //Reference to the table elemet
+    var table = document.querySelector(OUTPUT_TABLE);
+
+    //New row
+    var row = table.insertRow(table.rows.length);
+
+    //Inserts a new cell and a value in it
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = name;
+
+    //Inserts a second new cell and adds value in it
+    var cell2 = row.insertCell(1);
+    cell2.innerHTML = sum;
+
+    //Inserts a third new cell and creates a new date, then adds the date in the cell
+    var cell3 = row.insertCell(2);
+    var date = new Date();
+    cell3.innerHTML = date.getDate() + '/' + date.getMonth() + ' - ' + date.getFullYear();;
+
+}
+
+/*
+    The 'main' method starting the JS
+*/
 function init() {
     'use strict';
     addButtonListener();
@@ -145,4 +279,5 @@ function init() {
     addNameButtonListener();
 }
 
+//Starts everything
 init();
