d5763d6cf5184877016aee0f12e82db456cd923b
Oskarzito <oskar.kv@gmail.com>
2018-07-31 23:26:20 +0200

    3.4.3 Validerad och klar

--- 3.4.3/scripts/main.js
+++ 3.4.3/scripts/main.js
@@ -1,15 +1,38 @@
 
+/*
+    Programmet implementerar funktionaliteten till uppgift 3.4.3 Navigering
+
+    Samtliga saker nedan är gjorda:
+    * Att alla externa länkar på en webbsida öppnas i ett nytt fönster med hjälp
+      av automatiskt införande av attributet target="_blank" för dessa länkar
+
+    * En animerad navigationslist med drop-down menyer eller liknande
+*/
 
 $(document).ready(function () {
+    //Selektorvariabler
     var MENU_BUTTON_SELECTOR = '[data-button="dropdown-menu"]';
     var DROPDOWN_MENU = '[data-menu="dropdown-menu"]';
+    var NAV_SELECTOR = '[data-nav="navigation-menu"]';
 
-    $(MENU_BUTTON_SELECTOR).hover(function () {
-        console.log('in');
-        $(DROPDOWN_MENU).slideDown(1000);
+    //Lyssnare på när man hovrar över menyelementet (knappen)
+    $(NAV_SELECTOR).hover(function () {
+        /*Om en animation håller på, gör inget. Detta är för att
+        förhindra att man snabbt drar in och ut muspekarer massvis
+        av gången och får animationer i efterhand.
+
+        Om en animation INTE håller på, animera ned menyn via slideDown() i en sekund*/
+        if(!($(DROPDOWN_MENU).is(':animated'))){
+            $(DROPDOWN_MENU).slideDown(1000);
+        }
+    }, function () {
+        //När musen lämnar menyn, animera upp menyn via slideUp() i en sekund
+        $(DROPDOWN_MENU).slideUp(1000);
     });
 
-    $(DROPDOWN_MENU).mouseleave(function (){
-        $(this).slideUp(1000);
-    });
+    /*Selektera alla länktaggar via jQuery. För alla länk-element (via 'each()'), sätt
+    deras target-attribut till '_blank' så öppnas dessa i nya fönster*/
+    $('a').each(function () {
+        $(this).attr('target', '_blank');
+    })
 });
