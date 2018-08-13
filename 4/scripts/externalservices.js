ce5ed994a7f6c00807c8202f9490f9e76623476f
Oskarzito <oskar.kv@gmail.com>
2018-08-13 18:47:09 +0200

    4. Fortsatt

--- 4/scripts/externalservices.js
+++ 4/scripts/externalservices.js
@@ -139,9 +139,7 @@ function getData(apiCallUrl, callback) {
 }
 
 /**
-    Adderar lyssnare på lådmenyn uppe till vänster samt en knapplyssnare
-    till hela dokumentet. När man klickar på ESC-knappen stängs lådmenyn
-    (om den är öppen såklart. Annars händer inget)
+    Adderar lyssnarfunktionalitet på lådmenyn uppe till vänster.
 */
 function setUpDrawerMenu() {
     const ESC_KEY = 27;
@@ -149,15 +147,18 @@ function setUpDrawerMenu() {
     const DRAWER_MENU_SELECTOR = '[data-menu="drawer-menu"]';
     const CLOSE_DRAWER_SELECTOR = '[data-button="close-drawer"]';
 
+    //Klicklyssnare på knappen som öppnar menyn
     $(DRAWER_MENU_BUTTON_SELECTOR).on('click', function (event) {
         $(DRAWER_MENU_SELECTOR).css('width', '200px');
     });
 
+    //Klicklyssare på pil-ikonen som stänger menyn
     $(CLOSE_DRAWER_SELECTOR).on('click', function (event) {
         event.preventDefault();
         $(DRAWER_MENU_SELECTOR).css('width', '0');
     });
 
+    //När man klickar på ESC-knappen stängs lådmenyn(om den är öppen såklart. Annars händer inget)
     $(document.body).on('keyup', function (event) {
         event.preventDefault();
         if(event.keyCode === ESC_KEY) {
