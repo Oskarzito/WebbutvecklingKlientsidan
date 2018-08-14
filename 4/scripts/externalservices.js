b53acce64d98fe5746ffbc36f022079afe484fc5
Oskarzito <oskar.kv@gmail.com>
2018-08-14 18:04:52 +0200

    4. Lagt till Wikipedia-anrop (som funkar bannemig)

--- 4/scripts/externalservices.js
+++ 4/scripts/externalservices.js
@@ -1,10 +1,11 @@
 
 /*
     JS-filen implementerar funktionalitet för lite API-anrop.
-    Ett Google API sätter upp en google maps-karta. Via en lyssnare
+    Ett Google-API sätter upp en Google Maps-karta. Via en lyssnare
     på kartans markör hämtas latitud och longitud från kartan
     när man droppar markören på en plats. Sedan får man lite
-    väderinformation om platsen via ett API-anrop till openweathermaps.
+    diverse information om platsen via API-anrop till openweathermaps
+    samt Wikipedia.
 */
 
 $(document).ready(function () {
@@ -12,7 +13,6 @@ $(document).ready(function () {
     var marker = initMarker(map);
 
     const WEATHER_API_PATH = 'https://api.openweathermap.org/data/2.5/weather?units=metric&'
-    //var latlng = 'lat=59.334591&lon=18.063240'
     const WEATHER_API_KEY = '&APPID=f8a48d1a886d5c83eaf7ea5d8c0c6685'
 
     const WEATHER_FORM_INPUT_SELECTOR = '[data-input="place"]';
@@ -21,15 +21,24 @@ $(document).ready(function () {
     google.maps.event.addListener(marker, 'dragend', function () {
         //Hämtar positionen för markören
         var position = marker.getPosition();
-        console.log(position.lat() + ' - ' + position.lng());
         var lat = 'lat=' + position.lat();
         var lng = 'lon=' + position.lng();
+
         //Bygger URL:en för API-anrop och skickar till getData utan callback
         var url = WEATHER_API_PATH + lat + '&' + lng + WEATHER_API_KEY;
         getData(url, function (data) {
             updateOutput(data);
             //Sätt formulär-input-texten till platsens namn
             $(WEATHER_FORM_INPUT_SELECTOR).val(data.name);
+            
+            //Ta bort eventuella tabellrader innan nya skrivs ut
+            var $outputTableBody = $('[data-output="table-body-result"]');
+            $outputTableBody.empty();
+
+            /*Anropar Wikipedias API via koordinaten markören står på.
+             Resultatet blir information om närliggande platser som
+             skrivs ut som tabellrader på skärmen*/
+            queryWikipediaByGeo(position.lat(), position.lng());
         });
     });
 
@@ -39,7 +48,7 @@ $(document).ready(function () {
         event.preventDefault();
         var input = $(WEATHER_FORM_INPUT_SELECTOR).val();
 
-        //Bygg upp en URL av indatan och anropa getData
+        //Bygg upp en URL av indatan och anropa getData med callback
         var url = WEATHER_API_PATH + 'q=' + input + WEATHER_API_KEY;
         getData(url, function (data) {
             updateOutput(data, function (data) {
@@ -169,5 +178,103 @@ function setUpDrawerMenu() {
             $(DRAWER_MENU_SELECTOR).css('width', '0');
         }
     });
+}
 
+/**
+    Skickar ett API-anrop till Wikipedia med en koordinat som argument.
+    Responsen för anropen blir ett JSON-objekt innehållandes information
+    om närliggande geografiska platser till koordinaten
+    @param latitude Latitud-värdet för Google-Maps-markören
+    @param longitude Longitud-värdet för Google-Maps-markören
+*/
+function queryWikipediaByGeo(latitude, longitude) {
+    var searchParam = latitude + '|' + longitude;
+    const WIKI_GEO_URL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gsradius=10000&gscoord=';
+    //Skapar en url för API-androp
+    var searchUrl = WIKI_GEO_URL + searchParam;
+
+    $.ajax({
+        dataType: 'jsonp',
+        url: searchUrl,
+        success: handleWikipediaGeoResponse,
+        error: function () {
+            console.log('Something went wrong when fetching data from Wikipedia');
+        }
+    });
+}
+
+/**
+    Hanterar JSON-responsen från ett anrop till wikipeda
+    @param data JSON-objekt innehållandes data om närliggande platser till en koordinat
+*/
+function handleWikipediaGeoResponse(data) {
+    var resultArray = data.query.geosearch;
+
+    /*För samtliga resultat (geografiska platser) i arrayen, skapa en URL för
+    platsen och skriv ut som en tabellrad på skärmen*/
+    resultArray.forEach(function (element) {
+        var pageId = element.pageid;
+        var name = element.title;
+        var distance = element.dist;
+        createWikipediaUrlFromPageIdAndPrintToScreen(pageId, name, distance);
+    });
+}
+
+/**
+    Tar ett wikipedia pageID och skapar en url till sidan
+    @param id PageID för en wikipedia-sida
+    @param name Namnet på närliggande platsen
+    @param dist Avståndet i meter till platsen från där markören släpptes på kartan
+*/
+function createWikipediaUrlFromPageIdAndPrintToScreen(id, name, dist) {
+    const WIKI_PAGEID_URL = 'https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&format=json&pageids=';
+
+    //Skapar en url för API-androp
+    var searchUrl = WIKI_PAGEID_URL + id;
+    $.ajax({
+        dataType: 'jsonp',
+        url: searchUrl,
+        success: function (data) {
+            //Data är ett JSON-objekt vars 'nyckel'-sökväg till url-adressen är den som tilldelas createdUrl
+            var createdUrl = data.query.pages[id].fullurl;
+            //Skapa tabell från output
+            createOutoutTableRows(name, createdUrl, dist);
+        },
+        error: function () {
+            console.log('Something went wrong when generating URL from pageID');
+        }
+    });
+
+}
+
+/**
+    Skapar tabellrader som representerar resultaten från närliggande platser
+    till där man droppade Google Maps-markören
+
+    @param name Namnet på närliggande platsen
+    @param link Länk till platsens wikipedia-sida
+    @param dist Avståndet i meter till platsen från där markören släpptes på kartan
+*/
+function createOutoutTableRows(name, link, dist) {
+    var $outputTableBody = $('[data-output="table-body-result"]');
+
+    //Skapar en länk med namn som text och href som wikipeda-url:en
+    var $nameAsLink = $('<a></a>');
+    $nameAsLink.text(name);
+    $nameAsLink.attr('target', '_blank');
+    $nameAsLink.prop('href', link);
+
+    //Skapar en tabellrad och adderar den sedan till tabellen
+    var $row = $('<tr></tr>');
+
+    var $cell1 = $('<td></td>');
+    $cell1.append($nameAsLink);
+
+    var $cell2 = $('<td></td>');
+    $cell2.text(dist);
+
+    $row.append($cell1);
+    $row.append($cell2);
+
+    $outputTableBody.append($row);
 }
